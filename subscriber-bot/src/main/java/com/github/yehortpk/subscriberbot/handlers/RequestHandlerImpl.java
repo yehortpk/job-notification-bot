9555a72745a4af069548b8011898fb0ee76a5add
yehortpk <e2000work@gmail.com>
2024-02-15 19:24:07 +0200

    Create text chunking for large message - Create getDividedLargeText methods for separate message based on max message length

--- subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/handlers/RequestHandlerImpl.java
+++ subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/handlers/RequestHandlerImpl.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public abstract class RequestHandlerImpl implements RequestHandler {
     @Autowired
     TelegramServiceUtil telegramServiceUtil;
+
+    private final int TG_MESSAGE_MAX_LENGTH = 4000;
 
     // todo remove previous message
 //    @Override
@@ -35,7 +40,60 @@ public abstract class RequestHandlerImpl implements RequestHandler {
         String text = sendMessage.getText();
         ReplyKeyboard markup = sendMessage.getReplyMarkup();
 
-        telegramServiceUtil.sendMessageWithMarkup(chatId, text, markup);
+        if(text.length() > TG_MESSAGE_MAX_LENGTH) {
+            List<String> dividedLargeText = getDividedLargeText(text);
+            for (String textPart : dividedLargeText) {
+                telegramServiceUtil.sendMessageWithoutMarkup(chatId, textPart);
+            }
+            telegramServiceUtil.sendMessageWithMarkup(chatId, "", markup);
+        } else {
+            telegramServiceUtil.sendMessageWithMarkup(chatId, text, markup);
+        }
+
+    }
+
+    // We need to find last close </a> tag to not crash tag parsing
+    // All the residuals from string after the tag are going to the next message
+    // todo check by TG_MESSAGE_MAX_LENGTH, it could be bigger
+    private List<String> getDividedLargeText(String text) {
+        List<String> result = new ArrayList<>();
+        int residuals = 0;
+        System.out.println("Text length: " + text.length());
+        System.out.println("Parts count = " + (text.length() / TG_MESSAGE_MAX_LENGTH + 1));
+        for (int i = 0; i < (text.length() / TG_MESSAGE_MAX_LENGTH); i++) {
+            int lowerBoundIndex = i * TG_MESSAGE_MAX_LENGTH - residuals;
+
+            int upperBoundIndex = (i + 1) * TG_MESSAGE_MAX_LENGTH;
+
+            String rawSubstring = text.substring(i * TG_MESSAGE_MAX_LENGTH, upperBoundIndex);
+            int lastClosedTagIndex = rawSubstring.lastIndexOf("</a>");
+            int closedTagIndex = i * TG_MESSAGE_MAX_LENGTH + (
+                    lastClosedTagIndex == -1?
+                            TG_MESSAGE_MAX_LENGTH:
+                            lastClosedTagIndex + 4
+            );
+
+            String taggedSubstring = text.substring(
+                    lowerBoundIndex,
+                    closedTagIndex
+            );
+
+            residuals = (i + 1) * TG_MESSAGE_MAX_LENGTH - closedTagIndex;
+
+            System.out.printf("Part %s: %s, [%s-%s] residuals: %s%n", i + 1, taggedSubstring.length(), lowerBoundIndex, closedTagIndex, residuals);
+//            System.out.printf("Part %s%n: %s%n", i + 1, taggedSubstring);
+            result.add(taggedSubstring);
+        }
+
+        int lastPartLowerBoundIndex = text.length() - (text.length() % TG_MESSAGE_MAX_LENGTH + residuals);
+        String lastSubstring = text.substring(
+                lastPartLowerBoundIndex
+        );
+        result.add(lastSubstring);
+
+        System.out.printf("Last part: length: %s%n", lastSubstring.length());
+
+        return result;
     }
 
     public abstract SendMessage handleRequest(UserRequestDTO userRequest);
