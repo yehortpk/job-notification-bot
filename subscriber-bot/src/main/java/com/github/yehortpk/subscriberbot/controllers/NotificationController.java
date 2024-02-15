87de2240cb1a969f9e51de8ac83458a246546657
yehortpk <e2000work@gmail.com>
2024-02-15 19:20:04 +0200

    Remove app dependence of kafka - Move kafka config to separate file - Make kafka config conditional

--- subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/controllers/NotificationController.java
+++ subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/controllers/NotificationController.java
@@ -3,9 +3,11 @@ package com.github.yehortpk.subscriberbot.controllers;
 import com.github.yehortpk.subscriberbot.dtos.VacancyNotificationDTO;
 import com.github.yehortpk.subscriberbot.services.NotifierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnProperty(name = "spring.kafka.bootstrap-servers")
 @Component
 public class NotificationController {
     @Autowired
