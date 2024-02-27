0f1ac423c1cef2698c32c6f2e3731ee61db13cd9
yehortpk <e2000work@gmail.com>
2024-02-27 15:18:15 +0200

    Add previous message removal for callback message - Add previous message removal after handler for one-message menu view

--- subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/handlers/callback/CallbackRequestHandlerImpl.java
+++ subscriber-bot/src/main/java/com/github/yehortpk/subscriberbot/handlers/callback/CallbackRequestHandlerImpl.java
@@ -2,10 +2,24 @@ package com.github.yehortpk.subscriberbot.handlers.callback;
 
 import com.github.yehortpk.subscriberbot.dtos.UserRequestDTO;
 import com.github.yehortpk.subscriberbot.handlers.StateRequestHandlerImpl;
+import com.github.yehortpk.subscriberbot.utils.TelegramServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public abstract class CallbackRequestHandlerImpl extends StateRequestHandlerImpl implements CallbackRequestHandler {
+    @Autowired
+    private TelegramServiceUtil telegramServiceUtil;
+
+    @Override
+    public void handle(UserRequestDTO userRequest) {
+        long chatId = userRequest.getUser().getChatId();
+        Integer messageId = userRequest.getUpdate().getCallbackQuery().getMessage().getMessageId();
+
+        super.handle(userRequest);
+        telegramServiceUtil.deleteMessage(chatId, messageId);
+    }
+
     @Override
     public boolean isApplicable(UserRequestDTO userRequest) {
         return super.isApplicable(userRequest) &&
