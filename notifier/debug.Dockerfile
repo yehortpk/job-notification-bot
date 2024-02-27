e3a2189d9b4f967c6bec968a4b1e0abe35a4e37a
yehortpk <e2000work@gmail.com>
2024-02-27 13:22:28 +0200

    Create component-based site parser - Add selenium library to requirements - Change docker build file for web chrome driver and Google Chrome browser - Add selenium configs - Create ComponentPageCompanySite abstract class for all the component-based sites - Create ComponentPageParser

--- notifier/debug.Dockerfile
+++ notifier/debug.Dockerfile
@@ -2,9 +2,22 @@ FROM gradle:8.6.0-jdk21 as notifier
 WORKDIR /app
 
 # Copy the Gradle build files (e.g., build.gradle and settings.gradle)
-COPY build.gradle settings.gradle /app/
-COPY src /app/src
+# COPY build.gradle settings.gradle /app/
+# COPY src /app/src
 
+# Install wget to download ChromeDriver
+RUN apt-get update && apt-get install -y wget gnupg unzip
+
+# Download and install ChromeDriver
+RUN wget https://chromedriver.storage.googleapis.com/109.0.5414.25/chromedriver_linux64.zip \
+    && unzip chromedriver_linux64.zip -d /usr/local/bin \
+    && rm chromedriver_linux64.zip
+
+RUN wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.119-1_amd64.deb \
+      && apt install -y /tmp/chrome.deb \
+      && rm /tmp/chrome.deb
+
+ENV PATH="/usr/local/bin:${PATH}"
 ENV SPRING_PROFILES_ACTIVE=debug
 
 # Build and run the application using Gradle
---
+++ notifier/src/main/java/com/github/yehortpk/notifier/entities/parsers/ComponentPageParser.java
@@ -0,0 +1,112 @@
+package com.github.yehortpk.notifier.entities.parsers;
+
+import lombok.Setter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.springframework.lang.Nullable;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+@Setter
+public class ComponentPageParser extends PageParserImpl {
+    private String dynamicElementQuerySelector;
+    private String chromeDriverPath;
+    private String chromePath;
+    private volatile boolean streamIsClosed = false;
+
+    public ComponentPageParser(String pageUrl, int pageId) {
+        super(pageUrl, pageId);
+    }
+
+    @Override
+    public Document parsePage(Proxy proxy) {String[] proxyData = retrieveDataFromProxy(proxy);
+        ChromeOptions chromeOptions = getChromeOptions();
+        chromeOptions.addArguments(String.format("--proxy-server=%s:%s", proxyData[0], proxyData[1]));
+        return parsePage(chromeOptions, proxy);
+    }
+
+    @Override
+    public Document parsePage() {
+        return parsePage(getChromeOptions(), null);
+    }
+
+    protected Document parsePage (ChromeOptions chromeOptions, @Nullable Proxy proxy) {
+        // Check if proxy capable of load page, then with webdriver (memory efficiently)
+        try {
+            Jsoup.connect(pageUrl)
+                    .proxy(proxy)
+                    .userAgent("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316" +
+                            " Firefox/3.6.2")
+                    .headers(headers)
+                    .data(data)
+                    .method(parseMethod)
+                    .execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Some bug with unclosed stream in selenium https://github.com/SeleniumHQ/selenium/issues/13096
+        if (!streamIsClosed) {
+
+            synchronized (ComponentPageParser.class) {
+                WebDriver driver = getWebDriver(chromeOptions);
+
+                String finalPageUrl = pageUrl;
+                if (!data.isEmpty()) {
+                    finalPageUrl += "?" + data.entrySet().stream()
+                            .map(entry -> entry.getKey() + "=" + entry.getValue())
+                            .collect(Collectors.joining("&"));
+                }
+
+                driver.get(finalPageUrl);
+
+                Wait<WebDriver> wait = new FluentWait<>(driver)
+                        .withTimeout(Duration.of(20, ChronoUnit.SECONDS))
+                        .pollingEvery(Duration.of(5, ChronoUnit.SECONDS))
+                        .ignoring(NoSuchElementException.class)
+                        .ignoring(IOException.class);
+                wait.until(dr -> dr.findElement(By.cssSelector(dynamicElementQuerySelector)));
+
+                final String htmlContent = driver.getPageSource();
+                streamIsClosed = true;
+                driver.quit();
+
+                return Jsoup.parse(htmlContent);
+            }
+        }
+
+        return null;
+    }
+
+    private String[] retrieveDataFromProxy(Proxy proxy) {
+        InetSocketAddress address = (InetSocketAddress) proxy.address();
+
+        return new String[]{address.getHostString(), String.valueOf(address.getPort())};
+    }
+
+    private WebDriver getWebDriver(ChromeOptions chromeOptions) {
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+        return new ChromeDriver(chromeOptions);
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--disable-dev-shm-usage", "--no-sandbox");
+        chromeOptions.setBinary(chromePath);
+
+        return chromeOptions;
+    }
+}
