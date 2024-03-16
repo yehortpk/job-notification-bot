b15a011e68b06ecdcce786ae969dd97289a5c5ab
yehortpk <e2000work@gmail.com>
2024-03-16 10:46:43 +0200

    Initialize proxy loading when service loaded

--- notifier/src/main/java/com/github/yehortpk/notifier/services/ProxyService.java
+++ notifier/src/main/java/com/github/yehortpk/notifier/services/ProxyService.java
@@ -1,6 +1,7 @@
 package com.github.yehortpk.notifier.services;
 
 import com.github.yehortpk.notifier.models.ProxyDTO;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,13 +27,19 @@ public class ProxyService {
     private final List<ProxyDTO> proxies = new ArrayList<>();
     private static final ThreadLocal<List<Integer>> threadLocalParam = ThreadLocal.withInitial(ArrayList::new);
 
-    public void loadProxies() throws IOException {
-        Document page = Jsoup.connect(proxySiteURL)
-                .userAgent("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316" +
-                        " Firefox/3.6.2")
-                .header("Content-Language", "en-US")
-                .timeout(60 * 1000)
-                .get();
+    @PostConstruct
+    private void loadProxies(){
+        Document page;
+        try {
+            page = Jsoup.connect(proxySiteURL)
+                    .userAgent("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316" +
+                            " Firefox/3.6.2")
+                    .header("Content-Language", "en-US")
+                    .timeout(60 * 1000)
+                    .get();
+        } catch (IOException e) {
+            throw new RuntimeException("Cant load proxies: " + e.getMessage());
+        }
         List<Element> rows = page.select(".fpl-list tbody > tr");
         for (Element row : rows) {
             List<Element> columns = row.select("td");
