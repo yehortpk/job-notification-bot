9618f93cda515d7986ea01d5e2b90e8bf9cb1055
yehortpk <e2000work@gmail.com>
2024-03-19 18:40:59 +0200

    Remove scheduling from project. Fix hardcoded data in properties

--- notifier/src/main/java/com/github/yehortpk/notifier/NotifierApplication.java
+++ notifier/src/main/java/com/github/yehortpk/notifier/NotifierApplication.java
@@ -2,7 +2,6 @@ package com.github.yehortpk.notifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class NotifierApplication {
