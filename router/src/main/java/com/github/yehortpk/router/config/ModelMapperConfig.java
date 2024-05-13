d3f912c6c897c4b87ad07f722c459b4c5ba8e842
yehortpk <e2000work@gmail.com>
2024-05-13 16:00:01 +0300

    Fix bug with wrong mapping in VacancyShortDTO

--- router/src/main/java/com/github/yehortpk/router/config/ModelMapperConfig.java
+++ router/src/main/java/com/github/yehortpk/router/config/ModelMapperConfig.java
@@ -6,6 +6,7 @@ import com.github.yehortpk.router.models.filter.Filter;
 import com.github.yehortpk.router.models.filter.FilterShortInfoDTO;
 import com.github.yehortpk.router.models.vacancy.Vacancy;
 import com.github.yehortpk.router.models.vacancy.VacancyDTO;
+import com.github.yehortpk.router.models.vacancy.VacancyShortDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
@@ -34,6 +35,13 @@ public class ModelMapperConfig {
                 modelMapper.createTypeMap(Vacancy.class, VacancyDTO.class);
 
         vacancyToVacancyDTOMap.addMapping((vacancy -> vacancy.getCompany().getCompanyId()), VacancyDTO::setCompanyID);
+
+        TypeMap<Vacancy, VacancyShortDTO> vacancyToVacancyShortDTOMap =
+                modelMapper.createTypeMap(Vacancy.class, VacancyShortDTO.class);
+
+        vacancyToVacancyShortDTOMap.addMapping((vacancy -> vacancy.getCompany().getCompanyId()), VacancyShortDTO::setCompanyId);
+        vacancyToVacancyShortDTOMap.addMapping((Vacancy::getTitle), VacancyShortDTO::setVacancyTitle);
+        vacancyToVacancyShortDTOMap.addMapping((Vacancy::getLink), VacancyShortDTO::setVacancyURL);
 
         return modelMapper;
     }
