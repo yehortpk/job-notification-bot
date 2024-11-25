a8845836e2e5e1df4ccd907281e57ed0e0e692be
yehortpk <e2000work@gmail.com>
2024-11-25 17:27:10 +0200

    Fix N+1 error in CompanyRepository

--- router/src/main/java/com/github/yehortpk/router/repositories/CompanyRepository.java
+++ router/src/main/java/com/github/yehortpk/router/repositories/CompanyRepository.java
@@ -2,12 +2,16 @@ package com.github.yehortpk.router.repositories;
 
 import com.github.yehortpk.router.models.client.Client;
 import com.github.yehortpk.router.models.company.Company;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Long>, JpaSpecificationExecutor<Client> {
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = {"companyData", "companyHeaders"})
+    @NonNull
+    List<Company> findAll();
     Company findByCompanyId(long companyId);
-    List<Company> findByIsEnabledTrue();
 }
