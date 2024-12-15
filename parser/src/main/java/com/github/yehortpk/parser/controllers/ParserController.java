3bb1c93d4b9c316462e84f18754b2a7b2f31a9b7
yehortpk <e2000work@gmail.com>
2024-12-15 15:33:31 +0200

    Add parsed already started condition check in /parser request

--- parser/src/main/java/com/github/yehortpk/parser/controllers/ParserController.java
+++ parser/src/main/java/com/github/yehortpk/parser/controllers/ParserController.java
@@ -2,6 +2,8 @@ package com.github.yehortpk.parser.controllers;
 
 import com.github.yehortpk.parser.ParserRunner;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,14 @@ public class ParserController {
     private final ParserRunner parserRunner;
 
     @PostMapping("/start")
-    public void startParsing() {
-        parserRunner.start();
+    public ResponseEntity<String> startParsing() {
+        if (!parserRunner.isAlive()) {
+            parserRunner.start();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Parsing has been started.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Parsing has already been started.");
+        }
     }
 }
