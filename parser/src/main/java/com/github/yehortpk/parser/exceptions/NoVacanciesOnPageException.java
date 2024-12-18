78502cf60d3d40fe5fcadbf8e3ce75388369a9b2
yehortpk <e2000work@gmail.com>
2024-12-18 13:38:33 +0200

    Add pageId field to NoVacanciesOnPageException for further extraction

--- parser/src/main/java/com/github/yehortpk/parser/exceptions/NoVacanciesOnPageException.java
+++ parser/src/main/java/com/github/yehortpk/parser/exceptions/NoVacanciesOnPageException.java
@@ -1,7 +1,15 @@
 package com.github.yehortpk.parser.exceptions;
 
+import lombok.Getter;
 import lombok.experimental.StandardException;
 
+@Getter
 @StandardException
-public class NoVacanciesOnPageException extends Exception{
+public class NoVacanciesOnPageException extends Exception {
+    private int pageId;
+
+    public NoVacanciesOnPageException(int pageId) {
+        super(String.format("No vacancies on page %s", pageId));
+        this.pageId = pageId;
+    }
 }
