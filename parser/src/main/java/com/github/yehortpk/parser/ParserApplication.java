7491739cc34d8d709ac6c404ec4dd372f0374ccb
yehortpk <e2000work@gmail.com>
2026-05-01 11:53:09 +0300

    fix(parser): add driver installation before app start

--- parser/src/main/java/com/github/yehortpk/parser/ParserApplication.java
+++ parser/src/main/java/com/github/yehortpk/parser/ParserApplication.java
@@ -2,11 +2,13 @@ package com.github.yehortpk.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.microsoft.playwright.Playwright;
 
 @SpringBootApplication
 public class ParserApplication {
 
 	public static void main(String[] args) {
+		Playwright.create().close();
 		SpringApplication.run(ParserApplication.class, args);
 	}
 
